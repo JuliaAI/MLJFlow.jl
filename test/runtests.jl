a61528ce4bb41678e3fad9bc7adf4ee6475fcd68
Jose Esparza <28990958+pebeto@users.noreply.github.com>
2023-10-12 21:24:47 -0500

    Improving testability by setting up an env variable with the mlflow server URI

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,16 @@ using MLFlowClient
 using MLJModelInterface
 using StatisticalMeasures
 
+# To run this tests, you need to set the URI of your MLFlow server. By default,
+# you can set:
+#
+# ENV["MLFLOW_URI"] = "http://localhost:5000"
+#
+# For more information, see https://mlflow.org/docs/latest/quickstart.html#view-mlflow-runs-and-experiments
+if ~haskey(ENV, "MLFLOW_URI")
+    error("WARNING: MLFLOW_URI is not set. To run this tests, you need to set the URI of your MLFlow server")
+end
+
 include("base.jl")
 include("types.jl")
 include("service.jl")
